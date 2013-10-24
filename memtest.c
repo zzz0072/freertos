be001e5e659fccaf044ec114d40375ab46af1197
Wen.Liao <zzz00072@gmail.com>
2013-10-24 22:26:49 +0800

    Correct and re-layout comments in prng()

--- memtest.c
+++ memtest.c
@@ -49,14 +49,14 @@ static int prng(void)
     /* taps: 16 14 13 11; characteristic polynomial: x^16 + x^14 + x^13 + x^11 + 1  */
     /* bit  = ((g_lfsr >> 0) ^ (g_lfsr >> 2) ^ (g_lfsr >> 3) ^ (g_lfsr >> 5) ) & 1; */
     /* g_lfsr =  (g_lfsr >> 1) | (bit << 15);                                       */
-    __asm__ ("mov r0, %[pre_lfsr]    \n"                                          \
-             "eor r1, r0, r0, lsr #2 \n" /* r1 = (g_lfsr >> 0) ^ (g_lfsr >> 2) */ \
-             "eor r1, r1, r0, lsr #3 \n" /* r1 = r1 ^ (g_lfsr >> 3) */            \
-             "eor r1, r1, r0, lsr #5 \n" /* r1 = r1 ^ (g_lfsr >> 5) */            \
-             "and r1, r1, #1         \n" /* bit = r1 = r1 & 1       */            \
-             "lsl r1, r1, #15        \n" /* bit << 15               */            \
-             "orr r1, r1, r0, lsr #1 \n" /* g_lfsr = r1 | r2        */            \
-             "mov %[result], r1      \n" /* Store result            */            \
+    __asm__ ("mov r0, %[pre_lfsr]    \n" /* r0 = g_lfsr             */ \
+             "eor r1, r0, r0, lsr #2 \n" /* r1 = r0 ^ (g_lfsr >> 2) */ \
+             "eor r1, r1, r0, lsr #3 \n" /* r1 = r1 ^ (g_lfsr >> 3) */ \
+             "eor r1, r1, r0, lsr #5 \n" /* r1 = r1 ^ (g_lfsr >> 5) */ \
+             "and r1, r1, #1         \n" /* bit = r1 = r1 & 1       */ \
+             "lsl r1, r1, #15        \n" /* bit << 15               */ \
+             "orr r1, r1, r0, lsr #1 \n" /* g_lfsr = r1 | r2        */ \
+             "mov %[result], r1      \n" /* Store result            */ \
              : [result]  "=r" (g_lfsr)   /* Output              */
              : [pre_lfsr] "r" (g_lfsr)   /* Input               */
              : "r0", "r1"                /* clobbered registers */);
